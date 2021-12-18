8889f9af9519125ef11e41cac8651026fefbeb93
Julien <julien.mousqueton@computacenter.com>
2021-12-18 18:47:36 +0100

    make a better debug information

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from configparser import ConfigParser
 
 config = ConfigParser(interpolation=None)
 config.read('config.cfg')
-log = config.get('Bot','log')
+debug = config.get('Bot','DEBUG')
 API_key = config.get('Twitter', 'API_Key')
 API_key_secret = config.get('Twitter', 'API_Key_secret')
 access_token = config.get('Twitter', 'Access_token')
@@ -14,24 +14,32 @@ hashtags = config.get('Bot', 'Hashtags')
 hashtags = hashtags.split(',')
 LastRun = config.get('Bot','LastRun')
 now = datetime.utcnow().isoformat(timespec='seconds')+'Z'
+
+def logger(s):
+    if str(debug) == 'True':
+        print(s)
+
+logger('Debug		: '+ debug)
+logger('Date 		: '+ now)
+
 if LastRun == '': 
+	logger("First Run")
 	config.set('Bot', 'LastRun', str(now)) 
 	with open("config.cfg", 'w') as f:
 	    config.write(f)
 	exit()
 
+logger('Last Run	: '+LastRun)
 config.set('Bot', 'LastRun', str(now)) 
 with open("config.cfg", 'w') as f:
 	config.write(f)
-
+logger('---')
 client = tweepy.Client(bearer_token=Bearer_token.replace("'", ""), consumer_key=API_key.replace("'", ""), consumer_secret=API_key_secret.replace("'", ""), access_token=access_token.replace("'", ""), access_token_secret=access_token_secret.replace("'", ""))
 for hash in hashtags:
 	tweets=client.search_recent_tweets(query='#' + hash.replace("'", "").replace(' ', '') + ' -RT',start_time=LastRun, tweet_fields=['context_annotations', 'created_at'], max_results=50)
 	if not tweets.data: 
-		if log == 'True': 
-			print('no new tweet for #'+hash.replace("'", "").replace(' ', ''))
+		logger('No new tweet for #'+hash.replace("'", "").replace(' ', ''))
 	else:
 		for tweet in tweets.data:
-			if log == 'True': 
-				print('#' + hash.replace("'", "").replace(' ', '') + ' ' + str(tweet.id) + ' ' + str(tweet.created_at))
-			client.retweet(str(tweet.id))
+			logger('#' + hash.replace("'", "").replace(' ', '') + ' ' + str(tweet.id) + ' ' + str(tweet.created_at))
+			# client.retweet(str(tweet.id))
