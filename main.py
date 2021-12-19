21c83d2e8d8a07202c21f672ff125bb3c71c9e04
Julien <julien.mousqueton@computacenter.com>
2021-12-19 09:55:58 +0100

    correct typo

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ for hash in hashtags:
 		for tweet in tweets.data:
 			if "RT @" not in tweet.text:
 				logger('[+] #' + hash.replace("'", "").replace(' ', '') + ' ' + str(tweet.id) + ' ' + str(tweet.created_at))
-				# client.retweet(str(tweet.id))
+				client.retweet(str(tweet.id))
 			else:
 				logger('[-] tweet '+ str(tweet.id) + ' is a RT')
 # END 
