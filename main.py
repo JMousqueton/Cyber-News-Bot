e261252e5aae6723609a77b3e8e8519315c31559
Julien <julien.mousqueton@computacenter.com>
2021-12-19 09:55:19 +0100

    optimization

--- main.py
+++ main.py
@@ -55,13 +55,16 @@ with open("config.cfg", 'w') as f:
 logger('---')
 client = tweepy.Client(bearer_token=Bearer_token.replace("'", ""), consumer_key=API_key.replace("'", ""), consumer_secret=API_key_secret.replace("'", ""), access_token=access_token.replace("'", ""), access_token_secret=access_token_secret.replace("'", ""),wait_on_rate_limit=True)
 for hash in hashtags:
-	query = '#' + hash.replace("'", "").replace(' ', '') + ' exclude:replies exclude:retweets lang:' + lang.replace("'", "").replace(' ', '')
+	query = '#' + hash.replace("'", "").replace(' ', '') + ' -is:retweet -is:reply lang:' + lang.replace("'", "").replace(' ', '')
 	logger('Query : ' +  query)
 	tweets=client.search_recent_tweets(query=query,start_time=LastRun, tweet_fields=['context_annotations', 'created_at'], max_results=50)
 	if not tweets.data: 
 		logger('[-]No new tweet for #'+hash.replace("'", "").replace(' ', ''))
 	else:
 		for tweet in tweets.data:
-			logger('[+] #' + hash.replace("'", "").replace(' ', '') + ' ' + str(tweet.id) + ' ' + str(tweet.created_at))
-			client.retweet(str(tweet.id))
+			if "RT @" not in tweet.text:
+				logger('[+] #' + hash.replace("'", "").replace(' ', '') + ' ' + str(tweet.id) + ' ' + str(tweet.created_at))
+				# client.retweet(str(tweet.id))
+			else:
+				logger('[-] tweet '+ str(tweet.id) + ' is a RT')
 # END 
