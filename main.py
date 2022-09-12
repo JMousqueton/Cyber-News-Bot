871c7101150263a0561ab14ea7fcedef762336d6
Julien <julien.mousqueton@computacenter.com>
2022-09-12 02:00:23 +0200

    remove another #

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ with open("config.cfg", 'w') as f:
 logger('---')
 client = tweepy.Client(bearer_token=Bearer_token.replace("'", ""), consumer_key=API_key.replace("'", ""), consumer_secret=API_key_secret.replace("'", ""), access_token=access_token.replace("'", ""), access_token_secret=access_token_secret.replace("'", ""),wait_on_rate_limit=True)
 for hash in hashtags:
-	query = '#' + hash.replace("'", "").replace(' ', '') + ' -#imessage -#twitterdown -#CheatSheet -#100DaysOfCode -#Geek -is:retweet -is:reply lang:' + lang.replace("'", "").replace(' ', '')
+	query = '#' + hash.replace("'", "").replace(' ', '') + ' -#Roblox -#imessage -#twitterdown -#CheatSheet -#100DaysOfCode -#Geek -is:retweet -is:reply lang:' + lang.replace("'", "").replace(' ', '')
 	logger('Query : ' +  query)
 	tweets=client.search_recent_tweets(query=query,start_time=LastRun, tweet_fields=['context_annotations', 'created_at'], max_results=50)
 	if not tweets.data: 
